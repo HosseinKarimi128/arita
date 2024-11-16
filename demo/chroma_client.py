1473250edb75a4b64dea016775aa3c5ca72fe2cc
Amir Mohammad Piran <95971260+Amirmohammadpiran@users.noreply.github.com>
2024-11-16 15:03:13 +0330

    implementation of chroma client
    this client interacts with containarized chromadb

---
+++ demo/chroma_client.py
@@ -0,0 +1,75 @@
+import requests
+
+class chromaClient:
+
+    def __init__(self, url):
+
+        self.chroma_url = url
+
+    def insert_batch(self, facts: list[str], metadatas: list[dict]):
+
+        payload = {
+            "facts": facts,
+            "metadatas": metadatas
+        }
+        response = requests.post(f"{self.chroma_url}/insert-texts", json=payload)
+
+        if response.status_code == 200:
+            return response
+        else:
+            print(f"Failed to insert data: {response.text}")
+    
+
+    def insert_single(self, facts: str, metadatas: dict):
+
+        payload = {
+            "facts": [facts],
+            "metadatas": [metadatas]
+        }
+        response = requests.post(f"{self.chroma_url}/insert-texts", json=payload)
+
+        if response.status_code == 200:
+            return response
+        else:
+            print(f"Failed to insert data: {response.text}")
+    
+
+    def retrieve(self, query: str, 
+                 metadata=None, k=1, 
+                 maximum_distance=0.4):
+        
+        if metadata is None:
+            payload = {
+                "query": query,
+                "k" : k,
+                "maximum_distance" : maximum_distance
+            }
+        else:
+            payload = {
+                "query": query,
+                "metadata": metadata,
+                "k" : k,
+                "maximum_distance" : maximum_distance
+            }
+        response = requests.post(f"{self.chroma_url}/retrieve", json=payload)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to retrieve data: {response.text}")
+
+
+
+
+# client = chromaClient("http://0.0.0.0:8324")
+# response = client.retrieve("in time", {"type": "image"}, 3, 0.4)
+# response = client.retrieve(query="in time", k=3)
+
+# response = client.insert_single("A bunch of galactic battles between the empire and rebels. Lightsabers and jet fights everywhere",
+#                                  {"tools": "songsterr", "type": "music", "kw": "This is a kw about postman"})
+
+# response = client.insert_batch(["A bunch of galactic battles between the empire and rebels. Lightsabers and jet fights everywhere"],
+#                                  [{"tools": "songsterr", "type": "music", "kw": "This is a kw about postman"}])
+
+# print(response)
+
