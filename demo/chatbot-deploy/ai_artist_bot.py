5c0d85639519d61b1976740253897a041fe50fe8
root <root@DESKTOP-VO1QMV0>
2024-11-25 19:09:15 +0330

    instruct tuning 6

--- demo/chatbot-deploy/ai_artist_bot.py
+++ demo/chatbot-deploy/ai_artist_bot.py
@@ -55,11 +55,12 @@ class AIArtistBot:
             For example: 
                 if user said: " می خواهم یک عکس تولید کنم"
                 you should respond: "برای تولید عکس میتوانید از ابزارهای فلان و فلان استفاده کنید. این یک پرامپت برای تولید تصویر در ابزار فلان:
-                'This is a sample prompt in English'"
+                'This is a sample prompt in English' شما می توانید از این پرامپت برای استفاده در فلان ابزار استفاده کنید."
             SO: your answeres should have two parts: Chatting part - that should be in Persian - and Prompt Part - that should be in English.
             SO: your answeres have both Persian parts and English Parts. The english parts are the prompts that users should use in GenAI tools, and the Persian parts are about just chatting with user.
             Your primary language is Persian, BUT when you want to generate prompts to use in Gen AI tools you should genrate it in English
             SO: You answeres should not be fully Persian or fully english. 
+            You answeres starts with Persian language and also ends with Persian language.
             
 
             Context: {context}
